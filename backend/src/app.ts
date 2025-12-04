113bd1b7c5b9536175fbaced605395f544d2e2bc
i-nikotin-THEGAME <i_nikotin@thegame.design>
2025-12-04 03:21:12 +0300

    fix: Rate limiter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,11 +3,11 @@ import { nestCsrf } from 'ncsrf'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import helmet from 'helmet'
-import rateLimit from 'express-rate-limit'
 import 'dotenv/config'
 import express, { json, urlencoded } from 'express'
 import mongoose from 'mongoose'
 import path from 'path'
+import { apiLimiter, createLimiter } from './middlewares/rateLimit'
 import { DB_ADDRESS } from './config'
 import errorHandler from './middlewares/error-handler'
 import serveStatic from './middlewares/serverStatic'
@@ -15,18 +15,6 @@ import routes from './routes'
 
 const { PORT = 3000, ORIGIN_ALLOW } = process.env
 const app = express()
-
-const createLimiter = (windowMs: number, limit: number) => 
-    rateLimit({
-        windowMs,
-        max: limit,
-        standardHeaders: true,
-        legacyHeaders: false,
-        message: {
-            status: 429,
-            message: 'Слишком много запросов. Пожалуйста, попробуйте позже.'
-        }
-    })
 
 // 1. Безопасность и CORS
 app.use(cookieParser())
@@ -52,7 +40,10 @@ app.use(urlencoded({ extended: true }))
 app.use(json())
 
 // 4. Rate limiter
-app.use(createLimiter(15 * 60 * 1000, 100))
+app.use('/auth', apiLimiter)
+app.use('/products', apiLimiter)
+app.use('/orders', apiLimiter)
+app.use('/customers', apiLimiter)
 
 // 5. CORS для preflight запросов
 app.options('*', cors())
